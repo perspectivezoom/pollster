7a27924920a0ecb91666cd17dce93c0d263ce1e2
Michael Chen <m@perspectivezoom.com>
2012-07-30 11:20:56 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pollster::Application.config.secret_token = 'beefe51e95dee49b1ee4678289529047f7e4ebc4f02aa5c3be50d5cdc72f2af9d0cb57f82e4df7681371262b7aeccf7d03d397d0ed12c1c588b1c8adb4db12ee'
