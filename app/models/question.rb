b8bf81219e2ca49d46e9299644afa7598cf4b035
Michael Chen <m@perspectivezoom.com>
2012-08-20 14:43:09 -0700

    fixed some multiple choice validation and ui

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,7 +3,12 @@ class Question < ActiveRecord::Base
 
   validates_presence_of :question_text
   validates_associated :poll
+  validates :possible_responses, :presence => true, :if => :multiple_choice?
 
   belongs_to :poll
   has_many :answers, :dependent => :destroy
+
+  def multiple_choice?
+    question_type == 'multiple_choice'
+  end
 end
